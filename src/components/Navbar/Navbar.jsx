f3c99fe77f212518b748c3120ee3fa89f58f6c25
AishwaryaVijay24 <aishhh2409@gmail.com>
2024-03-20 15:34:55 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,6 @@ import logo from "./Logo.png";
 
 const Navbar = () => {
   return (
-<<<<<<< HEAD
-    <div className="Navbar_Container">
-      <div className="Nav_logo">
-        <img src={logo} className="logo" alt="" />
-=======
     <div className="Rectangle-135">
       <div className="navbar-content">
         <img src={logo} className="image-remofgvsfvebg-preview-2" alt="Logo" />
@@ -19,7 +14,6 @@ const Navbar = () => {
           <button className="complaint-btn">Services</button>
           <button className="complaint-btn">Contact Us</button>
         </div>
->>>>>>> 17c11c5ed21035b53ce4edd04298295b482839f5
       </div>
     </div>
   );
