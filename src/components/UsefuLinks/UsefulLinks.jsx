7ee5e84ac3264254be76f33037369f4b7a78cd8d
AishwaryaVijay24 <aishhh2409@gmail.com>
2024-03-21 01:21:01 +0530

    updated useful links

--- src/components/UsefuLinks/UsefulLinks.jsx
+++ src/components/UsefuLinks/UsefulLinks.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import './UsefulLinks.css'; 
+import './UsefulLinks.css';
 
 const UsefulLinks = () => {
   return (
     <div className="Group-107">
       <div className="BlueContainer">
         <div>
-      <div className="USEFUL-LINKS"><p>USEFUL LINKS</p></div>
+          <div className="USEFUL-LINKS"><p>USEFUL LINKS</p></div>
+        </div>
+        <div className="AllRectangles">
+          <div>
+            
+            <button className="FirstRectangle"></button>
+            <button className="SecondRectangle"></button>
+            <button className="ThirdRectangle"></button>
+            <button className="FourthRectangle"></button>
+            <button className="FifthRectangle"></button>
+            
+        </div>
+        <div>
+        </div>
       </div>
-      <div>
-     
-      <div className="FirstRectangle"></div>
-      
-      
-      <div className="SecondRectangle"></div>
-      
-      <div>
-      <div className="ThirdRectangle"></div>
-      </div>
-      <div>
-      <div className="FourthRectangle"></div>
-      </div>
-      <div>
-      <div className="FifthRectangle"></div>
-      </div>
-      </div>
-    <div>
-    </div>
     </div>
     </div>
   );
