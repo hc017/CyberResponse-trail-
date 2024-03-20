5b5f0ced2727c0e84a05edf69e8707eee08e412a
AishwaryaVijay24 <aishhh2409@gmail.com>
2024-03-20 16:05:59 +0530

    updated footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 import AllRoutes from './components/Navbar/AllRoutes';
-<<<<<<< HEAD
+
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 
-=======
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
->>>>>>> 81afe31aeeb21a7734063ad2be18bbb0e60de36a
+
 
 
 
