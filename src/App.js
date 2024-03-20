0d763cd5d611ee8ed044fbf5cf0ebc248d285268
AishwaryaVijay24 <132195414+AishwaryaVijay24@users.noreply.github.com>
2024-03-20 14:21:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 import AllRoutes from './components/Navbar/AllRoutes';
+import 
 
 
 function App() {
   return (
     <Router>
       <AllRoutes />
+      <Footer/>
     </Router>
   );
 }
