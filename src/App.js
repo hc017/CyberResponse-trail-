0e37ea9a8f35aa4d017922524ea023f5f0081b6f
hc <harshalchavan170@gmail.com>
2024-03-20 15:28:06 +0530

    Updated nav

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 import AllRoutes from './components/Navbar/AllRoutes';
-<<<<<<< HEAD
 import Navbar from './components/Navbar/Navbar';
-=======
 import Footer from './components/Footer/Footer';
->>>>>>> 23f2066d21f78a1c5d3c09f602fa03b5ffca1648
 
 
 function App() {
