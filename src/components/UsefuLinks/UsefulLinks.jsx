a6db808126625baede13fcd824229ec508e18846
AishwaryaVijay24 <aishhh2409@gmail.com>
2024-03-21 00:28:04 +0530

    updated useful links

---
+++ src/components/UsefuLinks/UsefulLinks.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './UsefulLinks.css'; 
+
+const UsefulLinks = () => {
+  return (
+    <div className="Group-107">
+      <div className="BlueContainer">
+        <div>
+      <div className="USEFUL-LINKS"><p>USEFUL LINKS</p></div>
+      </div>
+      <div>
+     
+      <div className="FirstRectangle"></div>
+      
+      
+      <div className="SecondRectangle"></div>
+      
+      <div>
+      <div className="ThirdRectangle"></div>
+      </div>
+      <div>
+      <div className="FourthRectangle"></div>
+      </div>
+      <div>
+      <div className="FifthRectangle"></div>
+      </div>
+      </div>
+    <div>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default UsefulLinks;
+
