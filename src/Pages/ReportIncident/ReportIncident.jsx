332b9af727056ae29cb565fd5f33f07b9257a26b
AishwaryaVijay24 <aishhh2409@gmail.com>
2024-03-28 18:30:23 +0530

    Incident Report

--- src/Pages/ReportIncident/ReportIncident.jsx
+++ src/Pages/ReportIncident/ReportIncident.jsx
@@ -2,26 +2,31 @@ import React from 'react';
 import './ReportIncident.css';
 import Em from "../../components/Emergency/Em";
 import { Link } from 'react-router-dom';
-const ReportIncident = () => {
-  return (
-    <div className='ReportContainer'>
-      <Em/>
-      <div className="IncidentBlock">
-        <div className="ToAlign">
-        <div className="Report-An-Incident-Immediately">
-          Report An Incident Immediately
-        </div>
-        <div className="ReportLine"></div>
-        </div>
-        <div className="ButtonCSS">
-        <Link to="/reportincident" type="button" className="ReportButton">
-            <span class="ReportText">I am a victim</span>
-            </Link>
-        </div>
-      </div>
-    </div>
-  )
-}
 
+const ReportIncident = () => {
+    return (
+        <div className='ReportContainer'>
+            <Em />
+            <div className="IncidentBlock">
+                <div className="ToAlign">
+                    <div className="Report-An-Incident-Immediately">
+                        Report An Incident Immediately
+                    </div>
+                    <div className="ReportLine"></div>
+                </div>
+                <div className="ButtonCSS">
+                    <Link to="/reportincident" className="ReportButton">
+            
+                        <span className="ReportText">I am a victim</span>
+                    </Link>
+                    <Link to="/reportincident" className="ReportButton">
+                        <span className="ReportText">Report</span>
+                    </Link>
+                </div>
+                <div className="Endline"></div>
+            </div>
+        </div>
+    );
+}
 
 export default ReportIncident;
