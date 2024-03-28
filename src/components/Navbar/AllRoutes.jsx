6b29912f63befa0017139e23a2261193acf427f1
AishwaryaVijay24 <aishhh2409@gmail.com>
2024-03-28 10:24:06 +0530

    Incident Report

--- src/components/Navbar/AllRoutes.jsx
+++ src/components/Navbar/AllRoutes.jsx
@@ -18,6 +18,7 @@ import UP from '../../Pages/RegisterComplaint/UpdatePhone/UP'
 import UserPhone from '../../Pages/RegisterComplaint/UserPhone/UserPhone'
 import UserLogin from '../../Pages/LoginRegister/UserLogin'
 import UserRegister from '../../Pages/LoginRegister/UserRegister'
+import ReportIncident from '../../Pages/Home/ReportIncident/ReportIncident'
 
 
 
@@ -38,8 +39,7 @@ const AllRoutes = () => {
     <Route path='/draftcomplaint' element={<DC/>}></Route>
     <Route path='/updatephone' element={<UP/>}></Route>
     <Route path='/userd' element={<UserPhone/>}></Route>
-
-
+    <Route path='/reportincident' element={<ReportIncident/>}></Route>
    </Routes>
   )
 }
