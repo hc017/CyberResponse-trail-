471891b6107ee75ceb2532897a5cff5a6f9514fb
hc <harshalchavan170@gmail.com>
2024-04-01 00:57:39 +0530

    Stay_informed page added

---
+++ src/Pages/StayInformed/S1.jsx
@@ -0,0 +1,35 @@
+import React from 'react' 
+import "./S1.css"
+
+const S1 = () => {
+  return (
+    <div className="S1_component">
+        <div class="title_box">
+            <h1 class="title_cyber_awarness">CyberCrime Awarness Campaign</h1>
+        </div>
+        <div class="I4C">
+            <div class="I4C_title">
+                <h2>About I4C</h2>
+                <p class="abt">The Indian Cybercrime Coordination Centre (I4C) is a significant initiative spearheaded by the Ministry of Home Affairs, Government of India, to address the growing threat of cybercrime comprehensively. Established on October 5th, 2018, and officially dedicated to the nation on January 10th, 2020, by the Honorable Home Minister, the I4C aims to bolster India's capabilities in combating cyber threats through enhanced coordination among various law enforcement agencies and stakeholders.</p>
+                <p class="abt">One of the primary objectives of the I4C is to foster collaboration and synergy among different agencies involved in tackling cybercrime. This involves streamlining communication channels, sharing intelligence and resources, and facilitating joint operations to effectively combat cyber threats across the nation. By promoting cooperation between law enforcement agencies at the local, state, and national levels, the I4C seeks to create a cohesive and coordinated approach to cybercrime prevention and investigation.</p>
+                <p class="abt">Furthermore, the I4C is focused on driving advancements in India's overall cybercrime-fighting capabilities. This entails investing in technology infrastructure, developing specialized skills and expertise among law enforcement personnel, and staying abreast of the latest trends and developments in the field of cybersecurity. Through targeted training programs, capacity-building initiatives, and the adoption of best practices, the I4C aims to strengthen the nation's resilience against cyber threats and ensure a safer digital environment for its citizens.</p>
+                <p class="abt">Moreover, the I4C places significant emphasis on enhancing citizen satisfaction levels by providing effective mechanisms for reporting cybercrimes and seeking assistance. By establishing dedicated helplines, online portals, and support services, the I4C aims to empower individuals and organizations to report cyber incidents promptly and receive timely assistance from law enforcement authorities. This proactive approach not only helps in addressing cybercrimes more efficiently but also instills confidence among citizens in the government's efforts to combat cyber threats.</p>
+                <p class="abt">The Indian Cybercrime Coordination Centre represents a pivotal step towards safeguarding India's cyberspace and protecting its citizens from the evolving challenges posed by cybercriminals. Through its collaborative approach, technological advancements, and citizen-centric initiatives, the I4C strives to build a robust framework for combating cyber threats and ensuring a secure digital future for the nation.</p>
+            </div>
+            <div>
+                <h2>Objectives of I4C</h2>
+                <ul>
+                    <li>To act as a nodal point to curb Cybercrime in the country.</li>
+                    <li>To strengthen the fight against Cybercrime committed against women and children.</li>
+                    <li>Facilitate easy filing Cybercrime related complaints and identifying Cybercrime trends and patterns.</li>
+                    <li>To act as an early warning system for Law Enforcement Agencies for proactive Cybercrime prevention and detection.</li>
+                    <li>Awareness creation among public about preventing Cybercrime.</li>
+                    <li>Assist States/UTs in capacity building of Police Officers, Public Prosecutors and Judicial Officers in the area of cyber forensic, investigation, cyber hygiene, cyber-criminology, etc.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default S1
---
+++ src/Pages/StayInformed/SI_one.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import "./SI_one.css"
+import S1 from './S1'
+
+
+const SI_one = () => {
+  return (
+    <div className='SI_comp'>
+          <div class="title_big">
+            <h1 class="title_cyber">CyberCrime</h1>
+        </div>
+        
+        <p class="cyber_intro">Within the framework of the Indian legal system, cybercrime remains a nebulous concept lacking a precise delineation. However, it broadly refers to a range of criminal activities perpetrated through the use of computers or computer networks, aimed at unlawfully accessing, transmitting, or manipulating data. This ambiguity is compounded by the dynamic nature of technology and the evolving tactics employed by cyber criminals, which challenge traditional legal definitions.</p>
+        <p class="cyber_intro">Cyber criminals, the architects of these nefarious deeds, exhibit a diverse range of technical skills and methodologies. They operate both independently and collaboratively, often forming clandestine networks within the recesses of the internet, such as the dark web, to plan and execute their illicit activities. This underground ecosystem serves as a breeding ground for various types of cyber offenders, including scammers, cyber stalkers, cyber terrorists, and black hat hackers, who exploit vulnerabilities in digital systems for personal gain or malicious intent.</p>
+        <p class="cyber_intro">Cybercrime manifests in multifaceted forms, falling broadly into two categories: crimes targeting computers and crimes employing computers as instruments. The former encompasses attacks aimed directly at compromising the integrity, confidentiality, or availability of computer systems, while the latter involves the use of computers to facilitate criminal endeavors, such as fraud, extortion, or trafficking. This distinction underscores the dual role that computers play in the commission of cybercrime, serving both as the target of malicious activity and as the conduit through which such activity is carried out.</p>
+        <p class="cyber_intro">Examples of cybercrime are as varied as they are numerous, reflecting the breadth and complexity of digital criminality. These include but are not limited to data breaches, wherein sensitive information is unlawfully accessed or exfiltrated from secure databases; malware attacks, which deploy malicious software to compromise the functionality or security of computer systems; phishing schemes, designed to deceive users into divulging personal or financial information through fraudulent means; identity theft, whereby personal or corporate identities are fraudulently appropriated for illicit purposes; hacking incidents, involving unauthorized access to computer systems or networks; cyberstalking and harassment, perpetrated through online platforms to intimidate or coerce victims; financial fraud, encompassing a range of illicit activities aimed at unlawfully obtaining monetary gain; and intellectual property theft, wherein proprietary information or creative works are unlawfully appropriated or exploited for commercial gain.</p>
+        <p class="cyber_intro">The ramifications of cybercrime extend far beyond the digital realm, exerting profound social, economic, and legal implications. Individuals, businesses, and governments alike are vulnerable to the disruptive and damaging effects of cyber attacks, which can result in financial loss, reputational damage, and even compromise national security. As technology continues to advance and cyber threats evolve, addressing the scourge of cybercrime demands a concerted effort from policymakers, law enforcement agencies, and the broader international community to enact robust legal frameworks, enhance cybersecurity measures, and foster greater collaboration in combating digital criminality.</p><br/><br/>
+
+        <h1 class="title">Activities</h1>
+        <div class="ag-format-container">
+            <div class="ag-courses_box">
+                <div class="ag-courses_item">
+                    <a href="/stayinformed_one" class="ag-courses-item_link">
+                        <div class="ag-courses-item_bg"></div>
+                        <p class="ag-courses-item_title">
+                            Crimes<br/>
+                            Awarness Campaigns
+                        </p>
+                    </a>
+                </div>
+
+                <div class="ag-courses_item">
+                    <a href="#" class="ag-courses-item_link">
+                    <div class="ag-courses-item_bg"></div>
+                        <p class="ag-courses-item_title">
+                            What you can do<br/>
+                            You Maybe Next!
+                        </p>
+                    </a>
+                </div>
+
+                <div class="ag-courses_item">
+                    <a href="#" class="ag-courses-item_link">
+                    <div class="ag-courses-item_bg"></div>
+                        <p class="ag-courses-item_title">
+                            Cyber Hygiene<br/>
+                            Wash your CyberHands
+                        </p>
+                    </a>
+                </div>
+
+                <div class="ag-courses_item">
+                    <a href="#" class="ag-courses-item_link">
+                    <div class="ag-courses-item_bg"></div>
+                        <p class="ag-courses-item_title">
+                            Cryptojacking<br/>
+                            Signs and Prevention
+                        </p>
+                    </a>
+                </div>
+
+                <div class="ag-courses_item">
+                    <a href="#" class="ag-courses-item_link">
+                    <div class="ag-courses-item_bg"></div>
+                        <p class="ag-courses-item_title">
+                            Frauds<br/>
+                            Financial Scams
+                        </p>
+                    </a>
+                </div>
+
+                <div class="ag-courses_item">
+                    <a href="#" class="ag-courses-item_link">
+                    <div class="ag-courses-item_bg"></div>
+                        <p class="ag-courses-item_title">
+                            Deepfakes<br/>
+                            Sexual Crimes
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <h1 class="title">Related News</h1>
+        <div class="container_blog">
+            <div class="row">
+            <div class="col-md-6 item">
+                <div class="item-in">
+                    <h4>Promised "High Paying" Jobs In Thailand, 25 Indians Forced Into Cyber Fraud</h4>
+                    <div class="seperator"></div>
+                    <p>The Mumbai police have arrested two persons after it emerged that over 25 Indians were lured with "high-paying" jobs in Thailand but taken to Laos, where they were forced to commit cyber fraud, an official said on Tuesday.</p>
+                    <a href="https://www.ndtv.com/india-news/promised-high-paying-jobs-in-thailand-25-indians-forced-into-cyber-fraud-2-arrested-5312985">Read More
+                        <i class="fa fa-long-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-md-6 item">
+                <div class="item-in">
+                    <h4>Probe Agency Arrests "Kingpin" Of ₹ 5,000-Crore Cyber Crime Case</h4>
+                    <div class="seperator"></div>
+                    <p>Ashish Kakkar, a "cyber fraudster" and resident of Greater Kailash in Delhi, was taken into custody from Holiday Inn hotel in Gurugram on March 2, the central agency said in a statement.</p>
+                    <a href="https://www.ndtv.com/india-news/probe-agency-arrests-kingpin-of-rs-5-000-crore-cyber-crime-case-5232957">Read More
+                        <i class="fa fa-long-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-md-6 item">
+                <div class="item-in">
+                    <h4>Work-From-Home, Part Time Jobs Scams Top Cyber Crimes In India</h4>
+                    <div class="seperator"></div>
+                    <p>The scamsters majorly approach victims through digital advertisements, channels of online messengers, and bulk SMS, a government official said.</p>
+                    <a href="https://www.ndtv.com/india-news/work-from-home-part-time-jobs-scams-top-cyber-crimes-in-india-4795948">Read More
+                        <i class="fa fa-long-arrow-right"></i>
+                    </a>
+                    </div>
+                </div>
+
+            <div class="col-md-6 item">
+                <div class="item-in">
+                    <h4>58% surge in cyber crimes in Pune; frauds and sexual exploitation primary motives of criminals: NCRB report</h4>
+                    <div class="seperator"></div>
+                    <p>Maharashtra recorded 8,249 cases of cybercrime in 2022 up from 5,562 cases in 2021.</p>
+                    <a href="https://indianexpress.com/article/cities/pune/58-surge-in-cyber-crimes-in-pune-frauds-and-sexual-exploitation-primary-motives-of-criminals-ncrb-report-9055227/">Read More
+                        <i class="fa fa-long-arrow-right"></i>
+                    </a>
+                    </div>
+                </div>
+
+            <div class="col-md-6 item">
+                <div class="item-in">
+                    <h4>12k Tiruvallur govt schoolkids trained in cyber safty</h4>
+                    <div class="seperator"></div>
+                    <p>Bal Raksha Bharat (BRB) trained school students on cyber safty </p>
+                    <a href="https://timesofindia.indiatimes.com/city/chennai/12k-tiruvallur-govt-schoolkids-trained-in-cyber-safety/articleshow/107806219.cms">Read More
+                        <i class="fa fa-long-arrow-right"></i>
+                    </a>
+                    </div>
+                </div>
+            </div>
+         </div>
+         </div>
+    
+       
+       
+  )
+}
+
+export default SI_one
