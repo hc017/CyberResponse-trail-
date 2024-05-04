06dfa38803b8f7105ace9dab0cb5e9d5654e74a6
AishwaryaVijay24 <aishhh2409@gmail.com>
2024-05-04 21:17:33 +0530

    Little changes

--- src/Pages/Home/Hero_section.jsx
+++ src/Pages/Home/Hero_section.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Hero_section.css";
-
+import { useNavigate } from "react-router";
 const Hero_section = () => {
+  const navigate= useNavigate();
+  const handleReport=()=>{
+    navigate('/reportincident')
+  }
   return (
 <div class="hero">
         <div class="hero-content">
             <h1 class="hero-title">Report Cybercrimes</h1>
             <p class="hero-subtitle">Empower a safer digital world</p>
             <div class="hero-buttons">
-                <button class="hero-button">REPORT <br /> INCIDENT</button>
+                <button class="hero-button" onClick={handleReport}>REPORT <br /> INCIDENT</button>
                 <button class="hero-button">HOW TO <br /> REPORT</button>
                 <button class="hero-button">STAY <br /> INFORMED</button>
             </div>
