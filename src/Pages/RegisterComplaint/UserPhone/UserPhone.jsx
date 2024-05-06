5fb715b662b4fc67e06d5cc232007cfe5b11e164
Abolee <abolee.ingole.btech2021@sitpune.edu.in>
2024-05-06 10:31:33 +0530

    userprofile

--- src/Pages/RegisterComplaint/UserPhone/UserPhone.jsx
+++ src/Pages/RegisterComplaint/UserPhone/UserPhone.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import "./UserPhone.css"
+import React from "react";
+import "./UserPhone.css";
 import Em from "../../../components/Emergency/Em";
 import UFP_red from "../../UserDetails/UserForm/UFP_red";
-import UP_bar from '../../UserDetails/Userprofilebar/UP_bar';
-import DCL from './DCLogo.png'; // Importing as default export
+import UP_bar from "../../UserDetails/Userprofilebar/UP_bar";
+import DCL from "./DCLogo.png"; // Importing as default export
+import { useAuth } from "../../../FirebaseCongfig/AuthContext";
 
 const UserPhone = () => {
+  const { currentUser } = useAuth();
+
   return (
     <div className="UserD_component">
-    <div className="UserD_innercomponent">
-      <Em />
-      <div className="UD_up_bar"></div>
+      <div className="UserD_innercomponent">
+        <Em />
+        <div className="UD_up_bar"></div>
         <UP_bar />
-      <div className="UD_up_bar"></div>
-      <UFP_red />
-      <div className="DC_innerform">
-      <img className='img_dc' src={DCL} alt="DC Logo" />
-      <h1 className='dc_text'>PAGE UNDER DEVELOPEMENT</h1>
-
-
+        <div className="UD_up_bar"></div>
+        <UFP_red />
+        <div className="UD_up_bar"></div>
+        <div className="UD_up_bar"></div>
+        <div className="innerform">
+          <div className="CW_susdetail_bar">
+            <p className="CW_sus_text">User Profile</p>
+          </div>
+          
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default UserPhone
+export default UserPhone;
