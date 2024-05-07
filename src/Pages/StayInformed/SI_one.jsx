0a4ad303d79b5b80561a5f605b0f8c6d55447b11
Abolee <abolee.ingole.btech2021@sitpune.edu.in>
2024-05-07 10:44:37 +0530

    stay informed updated

--- src/Pages/StayInformed/SI_one.jsx
+++ src/Pages/StayInformed/SI_one.jsx
@@ -1,104 +1,106 @@
-import React from 'react'
-import "./SI_one.css"
-import S1 from './S1'
-
+import React from "react";
+import "./SI_one.css";
+import S1 from "./S1";
 
 const SI_one = () => {
   return (
-    <div className='SI_comp'>
-          <div class="title_big">
-            <h1 class="title_cyber">CyberCrime</h1>
+    <div className="SI_comp">
+      <div class="title_big">
+        <h1 class="title_cyber">CyberCrime</h1>
+      </div>
+      <h1 class="title">Activities</h1>
+      <div class="ag-format-container">
+        <div class="ag-courses_box">
+          <div class="ag-courses_item">
+            <a href="/stayinformed_one" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+              <p class="ag-courses-item_title">
+                Crimes
+                <br />
+                Awarness Campaigns
+              </p>
+            </a>
+          </div>
+
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+              <p class="ag-courses-item_title">
+                What you can do
+                <br />
+                You Maybe Next!
+              </p>
+            </a>
+          </div>
+
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+              <p class="ag-courses-item_title">
+                Cyber Hygiene
+                <br />
+                Wash your CyberHands
+              </p>
+            </a>
+          </div>
+
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+              <p class="ag-courses-item_title">
+                Cryptojacking
+                <br />
+                Signs and Prevention
+              </p>
+            </a>
+          </div>
+
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+              <p class="ag-courses-item_title">
+                Frauds
+                <br />
+                Financial Scams
+              </p>
+            </a>
+          </div>
+
+          <div class="ag-courses_item">
+            <a href="#" class="ag-courses-item_link">
+              <div class="ag-courses-item_bg"></div>
+              <p class="ag-courses-item_title">
+                Deepfakes
+                <br />
+                Sexual Crimes
+              </p>
+            </a>
+          </div>
         </div>
-        
-        <p class="cyber_intro">Within the framework of the Indian legal system, cybercrime remains a nebulous concept lacking a precise delineation. However, it broadly refers to a range of criminal activities perpetrated through the use of computers or computer networks, aimed at unlawfully accessing, transmitting, or manipulating data. This ambiguity is compounded by the dynamic nature of technology and the evolving tactics employed by cyber criminals, which challenge traditional legal definitions.</p>
-        <p class="cyber_intro">Cyber criminals, the architects of these nefarious deeds, exhibit a diverse range of technical skills and methodologies. They operate both independently and collaboratively, often forming clandestine networks within the recesses of the internet, such as the dark web, to plan and execute their illicit activities. This underground ecosystem serves as a breeding ground for various types of cyber offenders, including scammers, cyber stalkers, cyber terrorists, and black hat hackers, who exploit vulnerabilities in digital systems for personal gain or malicious intent.</p>
-        <p class="cyber_intro">Cybercrime manifests in multifaceted forms, falling broadly into two categories: crimes targeting computers and crimes employing computers as instruments. The former encompasses attacks aimed directly at compromising the integrity, confidentiality, or availability of computer systems, while the latter involves the use of computers to facilitate criminal endeavors, such as fraud, extortion, or trafficking. This distinction underscores the dual role that computers play in the commission of cybercrime, serving both as the target of malicious activity and as the conduit through which such activity is carried out.</p>
-        <p class="cyber_intro">Examples of cybercrime are as varied as they are numerous, reflecting the breadth and complexity of digital criminality. These include but are not limited to data breaches, wherein sensitive information is unlawfully accessed or exfiltrated from secure databases; malware attacks, which deploy malicious software to compromise the functionality or security of computer systems; phishing schemes, designed to deceive users into divulging personal or financial information through fraudulent means; identity theft, whereby personal or corporate identities are fraudulently appropriated for illicit purposes; hacking incidents, involving unauthorized access to computer systems or networks; cyberstalking and harassment, perpetrated through online platforms to intimidate or coerce victims; financial fraud, encompassing a range of illicit activities aimed at unlawfully obtaining monetary gain; and intellectual property theft, wherein proprietary information or creative works are unlawfully appropriated or exploited for commercial gain.</p>
-        <p class="cyber_intro">The ramifications of cybercrime extend far beyond the digital realm, exerting profound social, economic, and legal implications. Individuals, businesses, and governments alike are vulnerable to the disruptive and damaging effects of cyber attacks, which can result in financial loss, reputational damage, and even compromise national security. As technology continues to advance and cyber threats evolve, addressing the scourge of cybercrime demands a concerted effort from policymakers, law enforcement agencies, and the broader international community to enact robust legal frameworks, enhance cybersecurity measures, and foster greater collaboration in combating digital criminality.</p><br/><br/>
-
-        <h1 class="title">Activities</h1>
-        <div class="ag-format-container">
-            <div class="ag-courses_box">
-                <div class="ag-courses_item">
-                    <a href="/stayinformed_one" class="ag-courses-item_link">
-                        <div class="ag-courses-item_bg"></div>
-                        <p class="ag-courses-item_title">
-                            Crimes<br/>
-                            Awarness Campaigns
-                        </p>
-                    </a>
-                </div>
-
-                <div class="ag-courses_item">
-                    <a href="#" class="ag-courses-item_link">
-                    <div class="ag-courses-item_bg"></div>
-                        <p class="ag-courses-item_title">
-                            What you can do<br/>
-                            You Maybe Next!
-                        </p>
-                    </a>
-                </div>
-
-                <div class="ag-courses_item">
-                    <a href="#" class="ag-courses-item_link">
-                    <div class="ag-courses-item_bg"></div>
-                        <p class="ag-courses-item_title">
-                            Cyber Hygiene<br/>
-                            Wash your CyberHands
-                        </p>
-                    </a>
-                </div>
-
-                <div class="ag-courses_item">
-                    <a href="#" class="ag-courses-item_link">
-                    <div class="ag-courses-item_bg"></div>
-                        <p class="ag-courses-item_title">
-                            Cryptojacking<br/>
-                            Signs and Prevention
-                        </p>
-                    </a>
-                </div>
-
-                <div class="ag-courses_item">
-                    <a href="#" class="ag-courses-item_link">
-                    <div class="ag-courses-item_bg"></div>
-                        <p class="ag-courses-item_title">
-                            Frauds<br/>
-                            Financial Scams
-                        </p>
-                    </a>
-                </div>
-
-                <div class="ag-courses_item">
-                    <a href="#" class="ag-courses-item_link">
-                    <div class="ag-courses-item_bg"></div>
-                        <p class="ag-courses-item_title">
-                            Deepfakes<br/>
-                            Sexual Crimes
-                        </p>
-                    </a>
-                </div>
+      </div>
+      <h1 class="title">Related News</h1>
+      <div class="container_blog">
+        <div class="row">
+          <div class="col-md-6 item">
+            <div class="item-in">
+              <h4>
+                Promised "High Paying" Jobs In Thailand, 25 Indians Forced Into
+                Cyber Fraud
+              </h4>
+              <div class="seperator"></div>
+              <p>
+                The Mumbai police have arrested two persons after it emerged
+                that over 25 Indians were lured with "high-paying" jobs in
+                Thailand but taken to Laos, where they were forced to commit
+                cyber fraud, an official said on Tuesday.
+              </p>
+              <a href="https://www.ndtv.com/india-news/promised-high-paying-jobs-in-thailand-25-indians-forced-into-cyber-fraud-2-arrested-5312985">
+                Read More
+                <i class="fa fa-long-arrow-right"></i>
+              </a>
             </div>
-        </div>
-
-
-
-
-        <h1 class="title">Related News</h1>
-        <div class="container_blog">
-            <div class="row">
-            <div class="col-md-6 item">
-                <div class="item-in">
-                    <h4>Promised "High Paying" Jobs In Thailand, 25 Indians Forced Into Cyber Fraud</h4>
-                    <div class="seperator"></div>
-                    <p>The Mumbai police have arrested two persons after it emerged that over 25 Indians were lured with "high-paying" jobs in Thailand but taken to Laos, where they were forced to commit cyber fraud, an official said on Tuesday.</p>
-                    <a href="https://www.ndtv.com/india-news/promised-high-paying-jobs-in-thailand-25-indians-forced-into-cyber-fraud-2-arrested-5312985">Read More
-                        <i class="fa fa-long-arrow-right"></i>
-                    </a>
-                </div>
-            </div>
-
-            <div class="col-md-6 item">
+          </div>
+          <div class="col-md-6 item">
                 <div class="item-in">
                     <h4>Probe Agency Arrests "Kingpin" Of ₹ 5,000-Crore Cyber Crime Case</h4>
                     <div class="seperator"></div>
@@ -142,12 +144,10 @@ const SI_one = () => {
                     </div>
                 </div>
             </div>
-         </div>
-         </div>
-    
-       
-       
-  )
-}
+        </div>
+      </div>
+    // </div>
+  );
+};
 
-export default SI_one
+export default SI_one;
